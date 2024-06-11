4e76d04bc37ba640020ab67ee1e59300cc24da81
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-10 22:13:40 -0300

    :clipboard: impl: now it can display the current pomodoro state and pause on button click

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,28 +1,66 @@
 #include "src/button/button.h"
+#include "src/pomodoro/pomodoro.h"
 
 #define BAUD 115200
+#define ONOFF_PIN 9
 
-#define ONOFF_PIN 9 // Button pin #1.
-#define BUTTON_DELAY 120
+#define BUTTON_DELAY 320
+#define WORK_DURATION 3000
+#define SMALL_BREAK_DURATION 1000
+#define LONG_BREAK_DURATION 3000
+#define MAX_WORK_SESSIONS 3
 
 button_o onoff(ONOFF_PIN);
+pomodoro_o pomodoro(WORK_DURATION, SMALL_BREAK_DURATION, LONG_BREAK_DURATION,
+                    MAX_WORK_SESSIONS);
 
 bool is_onoff_pressed;
+unsigned long timer = 0;
 
 void setup(void) {
   Serial.begin(BAUD);
 
-	pinMode(ONOFF_PIN, INPUT);
+  pinMode(ONOFF_PIN, INPUT);
 
-	onoff.on_click([](void){
-		delay(BUTTON_DELAY);
+  onoff.on_click([](void) {
+    const unsigned char curr_state = pomodoro.get_state();
 
-		Serial.println("Hello world!");
-	});
+    Serial.println(curr_state);
+
+    if (curr_state == POMODORO_STATE_OFF)
+      pomodoro.start(POMODORO_STATE_WORK);
+    else
+      pomodoro.stop();
+
+    delay(BUTTON_DELAY);
+  });
+
+  pomodoro.on_work([](void) {
+    const unsigned short time_left = pomodoro.get_time_left(timer);
+
+    Serial.print("Working: ");
+    Serial.println(time_left);
+  });
+
+  pomodoro.on_small_break([](void) {
+    const unsigned short time_left = pomodoro.get_time_left(timer);
+
+    Serial.print("Small break: ");
+    Serial.println(time_left);
+  });
+
+  pomodoro.on_long_break([](void) {
+    const unsigned short time_left = pomodoro.get_time_left(timer);
+
+    Serial.print("Long break: ");
+    Serial.println(time_left);
+  });
 }
 
 void loop(void) {
-	is_onoff_pressed = digitalRead(ONOFF_PIN);
+  is_onoff_pressed = digitalRead(ONOFF_PIN);
+  timer = millis();
 
-	onoff.refresh(is_onoff_pressed);
+  onoff.refresh(is_onoff_pressed);
+  pomodoro.refresh(timer);
 }
