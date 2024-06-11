06650ee744f2994a7a375e4fe425f90f47461bd2
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-11 09:54:27 -0300

    :sparkles: feature: now the code has a debug flag to activate small time spans, and it logs in hex

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -4,6 +4,7 @@
 #define BAUD 115200
 #define BUTTON_DELAY 320
 #define LOGGER
+/* #define DEBUG */
 
 #define ONOFF_BUTTON_PIN 9
 #define SKIP_BUTTON_PIN 8
@@ -11,10 +12,17 @@
 #define BREAK_LED_PIN 2
 #define LONG_BREAK_INDICATOR_LED_PIN 13
 
+#ifndef DEBUG
 #define WORK_DURATION 1500000
 #define SMALL_BREAK_DURATION 300000
 #define LONG_BREAK_DURATION 900000
 #define MAX_WORK_SESSIONS 3
+#else
+#define WORK_DURATION 5000
+#define SMALL_BREAK_DURATION 2000
+#define LONG_BREAK_DURATION 3000
+#define MAX_WORK_SESSIONS 3
+#endif
 
 button_o onoff(ONOFF_BUTTON_PIN);
 button_o skip_button(SKIP_BUTTON_PIN);
@@ -96,7 +104,14 @@ inline void log_application_state(void) {
   Serial.print("\t");
 
   const unsigned char state = pomodoro.get_state();
-  const unsigned short time_left = pomodoro.get_time_left(timer);
+  const unsigned long time_left = pomodoro.get_time_left(timer);
+  const unsigned long curr_duration = pomodoro.get_curr_duration();
+
+  char curr_duration_buff[9];
+  char time_left_buff[9];
+
+  sprintf(curr_duration_buff, "%0lX", curr_duration);
+  sprintf(time_left_buff, "%0lX", time_left);
 
   Serial.print(state == POMODORO_STATE_WORK          ? "wor"
                : state == POMODORO_STATE_SMALL_BREAK ? "sbr"
@@ -104,13 +119,9 @@ inline void log_application_state(void) {
                : state == POMODORO_STATE_OFF         ? "off"
                                                      : "unk");
   Serial.print("\t");
-  Serial.print(state == POMODORO_STATE_WORK          ? WORK_DURATION
-               : state == POMODORO_STATE_SMALL_BREAK ? SMALL_BREAK_DURATION
-               : state == POMODORO_STATE_LONG_BREAK  ? LONG_BREAK_DURATION
-               : state == POMODORO_STATE_OFF         ? 0
-                                                     : -1);
+  Serial.print(curr_duration_buff);
   Serial.print("\t");
-  Serial.print(time_left);
+  Serial.print(time_left_buff);
 
   Serial.println();
 #endif
